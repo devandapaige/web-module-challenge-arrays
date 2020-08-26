8a987b4d9494664651b17513a67316374b46ff15
Amanda Nelson <amanda.gaylord@gmail.com>
2020-08-25 21:39:36 -0400

    tasks #5 and #6 completed.

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ function getFlavorByIndex(originalFlavors, index) {
 getFlavorByIndex(originalFlavors, 2);
 getFlavorByIndex(originalFlavors, 7);
 
-/* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array. 
+/* Task 5 ✅: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array. 
 
 Your function should accept: 
 
@@ -112,11 +112,14 @@ Hint: You can use .splice() for this
 
 */
 
-function removeFlavorByName(/*code here*/) {
-  /*code here*/
+function removeFlavorByName(originalFlavors, flavor) {
+  let pos = originalFlavors.indexOf(flavor);
+  originalFlavors.splice(pos);
+  console.log(originalFlavors);
 }
+removeFlavorByName(originalFlavors, "Vanilla");
 
-/* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array. 
+/* Task 6 ✅ : With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array. 
 
 Your function should accept: 
 
@@ -124,9 +127,13 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/) {
-  /*code here*/
+function copy(originalFlavors) {
+  originalFlavors.push("Vanilla", "Vanilla Burnt Almond");
+  originalFlavors.shift();
+  const cloneOGFlavors = [...originalFlavors];
+  console.log(cloneOGFlavors);
 }
+copy(originalFlavors);
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
